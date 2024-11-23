aa6a411c9686a95bd75225129c8e444e439b4363
Harry Winser <harry@harrywinser.com>
2024-11-23 22:44:19 +0000

    Add ability to delete shape

--- src/components/SideBarComponent.tsx
+++ src/components/SideBarComponent.tsx
@@ -3,7 +3,8 @@ import "../App.css";
 import { MapContext } from "../context/MapContext";
 
 export const SideBarComponent: React.FC = () => {
-  const { enableDraw, disableDraw } = useContext(MapContext);
+  const { enableDraw, disableDraw, features, removeFeature } =
+    useContext(MapContext);
 
   const handleSelect = (value: string) => {
     enableDraw(value);
@@ -26,6 +27,16 @@ export const SideBarComponent: React.FC = () => {
         <option value="Circle">Circle</option>
       </select>
       <button onClick={handleDrawDisable}>Stop drawing</button>
+      {features.map((feature) => {
+        return (
+          <div key={feature.getGeometryName()}>
+            <div>{feature.getId()}</div>
+            <button onClick={() => removeFeature(feature.getId())}>
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/context/MapContext.tsx
+++ src/context/MapContext.tsx
@@ -1,15 +1,17 @@
-import { Map, View } from "ol";
-import { Type } from "ol/geom/Geometry";
+import { Feature, Map, View } from "ol";
+import Geometry, { Type } from "ol/geom/Geometry";
 import Draw from "ol/interaction/Draw";
 import TileLayer from "ol/layer/Tile";
 import VectorLayer from "ol/layer/Vector";
 
 import { OSM, Vector } from "ol/source";
+import VectorSource from "ol/source/Vector";
 import {
   createContext,
   ReactNode,
   useCallback,
   useEffect,
+  useReducer,
   useRef,
 } from "react";
 
@@ -17,32 +19,68 @@ interface MapContextInterface {
   map?: Map;
   enableDraw: (type: string) => void;
   disableDraw: () => void;
+  removeFeature: (id: string | number | undefined) => void;
+  features: Feature<Geometry>[];
 }
 
 const defaultValues = {
   map: undefined,
   enableDraw: () => null,
   disableDraw: () => null,
+  removeFeature: () => null,
+  features: [],
+};
+
+interface State {
+  features: Feature<Geometry>[];
+}
+
+const initialState: State = { features: [] };
+
+type ACTION =
+  | { type: "ADD_FEATURE"; payload: Feature<Geometry> }
+  | { type: "REMOVE_FEATURE"; payload: string };
+
+const mapReducer = (state: State, action: ACTION) => {
+  switch (action.type) {
+    case "ADD_FEATURE": {
+      return { ...state, features: [...state.features, action.payload] };
+    }
+    case "REMOVE_FEATURE": {
+      const filtered = state.features.filter(
+        (feature) => feature.getId() !== action.payload,
+      );
+      return { ...state, features: filtered };
+    }
+    default:
+      throw new Error("Unknown action type");
+  }
 };
 
 export const MapContext = createContext<MapContextInterface>(defaultValues);
 
 export const MapContextProvider = ({ children }: { children: ReactNode }) => {
   const mapRef = useRef<Map | undefined>(undefined);
+  const [state, dispatch] = useReducer(mapReducer, initialState);
 
   useEffect(() => {
     if (!mapRef.current) {
-      console.log("creating map");
       const tileLayer = new TileLayer({
         source: new OSM(),
       });
       tileLayer.set("id", "tileLayer");
 
       const source = new Vector({});
+      source.on("addfeature", (event) => {
+        const feature = event.feature;
+        if (feature) {
+          feature.setId(crypto.randomUUID());
+          feature && dispatch({ type: "ADD_FEATURE", payload: feature });
+        }
+      });
       const vectorLayer = new VectorLayer({
         source: source,
       });
-
       vectorLayer.set("harrysLayer", "vectorLayer");
 
       const map = new Map({
@@ -73,10 +111,28 @@ export const MapContextProvider = ({ children }: { children: ReactNode }) => {
     }
   };
 
+  const removeFeature = (id: string | number | undefined) => {
+    if (typeof id === "string") {
+      if (mapRef.current) {
+        const layer = mapRef.current
+          .getLayers()
+          .getArray()
+          .find((layer) => {
+            return layer.get("harrysLayer") === "vectorLayer";
+          });
+        if (layer) {
+          const vectorLayer = layer as VectorLayer;
+          const foundFeature = vectorLayer.getSource()?.getFeatureById(id);
+          vectorLayer.getSource()?.removeFeature(foundFeature);
+          dispatch({ type: "REMOVE_FEATURE", payload: id });
+        }
+      }
+    }
+  };
+
   const enableDraw = useCallback(
     (type: String) => {
       if (mapRef.current) {
-        console.log("Enabling draw");
         const layer = mapRef.current
           .getLayers()
           .getArray()
@@ -105,7 +161,9 @@ export const MapContextProvider = ({ children }: { children: ReactNode }) => {
   );
 
   return (
-    <MapContext.Provider value={{ enableDraw, disableDraw }}>
+    <MapContext.Provider
+      value={{ ...state, enableDraw, disableDraw, removeFeature }}
+    >
       {children}
     </MapContext.Provider>
   );
